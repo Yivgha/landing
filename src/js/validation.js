fc4e674345bae5cb277b577bc68c242081ed1cf4
Yevheniia P <90108689+Yivgha@users.noreply.github.com>
2023-04-30 10:43:39 +0300

    error img

---
+++ src/js/validation.js
@@ -0,0 +1,11 @@
+// import Notify from "notiflix";
+
+const input = document.querySelector("#mail");
+
+
+    input.setCustomValidity("This is a required field");
+console.log(input.validity);
+//    Notify.failure('Qui timide rogat docet negare');
+//    input.reportValidity();
+
+
