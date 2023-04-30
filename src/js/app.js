62781ecd260ec19dc8157397d61e979ee09e9d64
Yevheniia P <90108689+Yivgha@users.noreply.github.com>
2023-04-30 11:43:25 +0300

    build changes

---
+++ src/js/app.js
@@ -0,0 +1,5 @@
+lightGallery(document.getElementById('lightgallery'), {
+    plugins: [lgThumbnail, lgShare, lgRotate],
+    download: true,
+    controlers: true,
+});
--- src/js/validation.js
+++ src/js/validation.js
@@ -1,11 +1,7 @@
-// import Notify from "notiflix";npm install --save-dev html-webpack-plugin
 
-const input = document.querySelector("#mail");
+// const input = document.querySelector("#mail");
 
 
-    input.setCustomValidity("This is a required field");
-console.log(input.validity);
-//    Notify.failure('Qui timide rogat docet negare');
+//     input.setCustomValidity("This is a required field");
+// console.log(input.validity);
 //    input.reportValidity();
-
-
